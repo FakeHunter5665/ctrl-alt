b5a8958d97d03bd4f99d134e11e50dbcfa11056c
FakeHunter5665 <venkivenkatesh5665@gmail.com>
2024-05-05 04:36:45 +0530

    hii

--- js/script.js
+++ js/script.js
@@ -22,9 +22,18 @@ async function sendData() {
         });
 
         const responseData = await response.json();
-        console.log(responseData)
-        // Display response on the website
-        document.getElementById('result').innerHTML = responseData.compound;
-    } finally{}
-}
+        
+        console.log(responseData);
 
+        // Check if the compound value is negative
+        if (responseData.compound < 0) {
+            document.getElementById('result').innerHTML = "Mood not OK";
+        } else if ( responseData.compund <0.3 && responseData.compund > -0.3 )
+            { document.getElementById('result').innerHTML = "Mood is Nuetral";}
+        else {
+            document.getElementById('result').innerHTML = "Mood is pretty well";
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
